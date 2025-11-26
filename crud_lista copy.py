6fec11aae179c2d0cb45152b3c2bc65b8e837c19
rianssilva6-svg <rian.s.silva6@ba.estudante.senai.br>
2025-11-26 16:54:28 -0300

    Add files via upload

---
+++ crud_lista copy.py
@@ -0,0 +1,50 @@
+import os 
+os.system("cls || clear")
+
+# CRUD usando lista
+# Create = criar / salvar
+# Read = buscar / selecionar 
+# Update = atualizar / modificar
+# Delete = excluir
+
+# Criando uma lista.
+lista_clientes = []
+
+print ("\n1 - Create\n2 - Read\n3 - Update\n4 - Delete")
+print()
+while True:
+    escolha = input("Escolha: ")
+    match escolha:
+        case "1":
+            print ("Create - Adicionar / inserir")
+            nome = input("Digite um nome: ")
+            lista_clientes.append(nome)
+            print (f"O nome: {nome} foi inserindo com sucesso")
+
+        case "2":
+            print ("\nRead - ler / Mostrar")
+            print (lista_clientes)
+
+        case "3":
+            print ("\nUpdate - Atualizar / Alterar")
+            nome_para_atualizar = input("Digite o nome que deseja atualizar")
+            if nome_para_atualizar in lista_clientes:
+                novo_nome = input("Digite um novo nome: ")
+                indice = lista_clientes.index(nome_para_atualizar)
+                lista_clientes[indice] = novo_nome
+                print (f"O nome: {nome_para_atualizar} foi atualizado para {novo_nome}")
+            else:
+                print (f"O nome {nome_para_atualizar} não foi encontrado.") 
+
+            print (lista_clientes)
+
+        case "4":
+            print ("\nDelete - Excluir / Remover")
+            nome_para_excluir = input("Digite um nome para excluir: ")
+            if nome_para_excluir in lista_clientes:
+                lista_clientes.remove(nome_para_excluir)
+                print (f"{nome_para_excluir} foi excluido com sucesso.")
+            else:
+                print (f"O nome {nome_para_excluir} não foi encontrado.") 
+
+            print (lista_clientes)       
